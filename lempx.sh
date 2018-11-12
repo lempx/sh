f546bfd54e392480fd0de302864d956651316fd1
lempx <tengfeimake@gmail.com>
2018-11-12 15:53:41 +0800

    ok

---
+++ lempx.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+#@creat date 2018-11-11
+#@author Lempx.com
+
+mkdir -p /lempx/tmp
+mkdir -p /lempx/app
+mkdir -p /lempx/www
+
+#install common 
+yum install wget
+
+#install nignx 
+cd /lempx/tmp
+wget https://github.com/lempx/base/raw/master/nginx-1.14.1.tar.gz
+tar -xzvf nginx-1.14.1.tar.gz
+cd /lempx/tmp/nginx-1.14.1
+
+./configure --user=nobody --group=nobody --prefix=/lempx/app/nginx/1.14 \
+--with-http_stub_status_module \
+--with-http_gzip_static_module \
+--with-http_realip_module \
+--with-http_sub_module \
+--with-http_ssl_module
+
+make && make install
