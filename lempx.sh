dd0dda34dcd15fe885556ac571c05e0c915d294a
lempx <tengfeimake@gmail.com>
2018-11-12 20:25:44 +0800

    ok

--- lempx.sh
+++ lempx.sh
@@ -6,12 +6,17 @@ mkdir -p /lempx/tmp
 mkdir -p /lempx/app
 mkdir -p /lempx/www
 
+cd /lempx/tmp
 #install common 
 yum -y install wget
 yum -y install gcc automake autoconf libtool gcc-c++
 
+wget http://mirror.centos.org/centos/7/os/x86_64/Packages/pcre-8.32-17.el7.x86_64.rpm
+wget http://mirror.centos.org/centos/7/os/x86_64/Packages/pcre-devel-8.32-17.el7.x86_64.rpm
+rpm -ivh pcre-8.32-17.el7.x86_64.rpm
+rpm -ivh pcre-devel-8.32-17.el7.x86_64.rpm
+
 #install nignx 
-cd /lempx/tmp
 wget https://github.com/lempx/sh/raw/master/src/nginx-1.14.1.tar.gz
 tar -xzvf nginx-1.14.1.tar.gz
 cd /lempx/tmp/nginx-1.14.1
