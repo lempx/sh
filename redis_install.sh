19f8e7ab2c8362d3842414e02419c267317b7aa1
Ian.M <tengfeimake@gmail.com>
2018-11-13 12:23:58 +0800

    redis

---
+++ redis_install.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# https://redis.io/download
+
+mkdir -p /lempx/tmp
+mkdir -p /lempx/app
+ce /lempx/tmp
+
+wget http://download.redis.io/releases/redis-5.0.0.tar.gz
+tar xzf redis-5.0.0.tar.gz
+mv /lempx/tmp/redis-5.0.0 /lempx/app/
+cd /lempx/app/redis-5.0.0
+make
