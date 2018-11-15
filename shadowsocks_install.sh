10c2c9698186522e1e331c0b3fb2360404048c5e
Ian.M <tengfeimake@gmail.com>
2018-11-15 20:42:36 +0800

    ok

--- shadowsocks_install.sh
+++ shadowsocks_install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-yum install python-setuptools && easy_install pip
+yum -y install python-setuptools && easy_install pip
 pip install shadowsocks
 sudo ssserver -p 10443 -k password -m rc4-md5 --user nobody -d start
 # stop shadowsocks server
